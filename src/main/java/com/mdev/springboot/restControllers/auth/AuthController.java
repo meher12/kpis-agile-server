217975d40e39f856637c761202f7c0b4faf3f319
meher12 <meher.khemiri@gmail.com>
2022-04-27 15:25:01 +0200

    update test controller

--- src/main/java/com/mdev/springboot/restControllers/auth/AuthController.java
+++ src/main/java/com/mdev/springboot/restControllers/auth/AuthController.java
@@ -65,7 +65,7 @@ public class AuthController {
         UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
         List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
                 .collect(Collectors.toList());
-        
+
         return ResponseEntity.ok(
                 new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
     }
