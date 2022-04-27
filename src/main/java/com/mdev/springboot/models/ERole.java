c7fa54f3715c7781f35d2a01397692abd01b9a0a
meher12 <meher.khemiri@gmail.com>
2022-04-27 12:39:57 +0200

    update EROLE

--- src/main/java/com/mdev/springboot/models/ERole.java
+++ src/main/java/com/mdev/springboot/models/ERole.java
@@ -1,7 +1,5 @@
 package com.mdev.springboot.models;
 
 public enum ERole {
-    ROLE_PRODUCTOWNER, 
-    ROLE_SCRUMMASTER, 
-    ROLE_DEVELOPER;
+    ROLE_PRODUCTOWNER, ROLE_SCRUMMASTER, ROLE_DEVELOPER;
 }
