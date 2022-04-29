9a0752008c24ac7572687fcce09f742e416330ab
meher12 <meher.khemiri@gmail.com>
2022-04-29 11:34:05 +0200

    update bash script

--- crebuild.sh
+++ crebuild.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 #docker network prune
 CNAME_Server="kpis-agile-server"
-if [ "$(docker ps -qa -f name=$CNAME_Server)" ]; then
+CNAME_Clinet="kpis-agile-ui"
+CNAME_db="db-kpis-agile"
+if [ "$(docker ps -qa -f name=$CNAME_Server)" || "$(docker ps -qa -f name=$CNAME_Clinet)" || "$(docker ps -qa -f name=$CNAME_db)" ]; then
     echo ":: Found container - $CNAME_Server"
-    if [ "$(docker ps -q -f name=$CNAME_Server)" ]; then
-        echo ":: Stopping running container - $CNAME_Server"
-        docker stop $CNAME_Server;
+    if [ "$(docker ps -q -f name=$CNAME_Server)"  || "$(docker ps -q -f name=$CNAME_Clinet)" || "$(docker ps -q -f name=$CNAME_db)"]; then
+        echo ":: Stopping running container --"
+        docker-compose stop;
+        docker-compose up --no-recreate;
+    else
+      docker-compose up;    
     fi
     echo ":: Removing stopped container - $CNAME_Server"
     docker rm $CNAME_Server;
 fi
 
-CNAME_Clinet="kpis-agile-ui"
-if [ "$(docker ps -qa -f name=$CNAME_Clinet)" ]; then
-    echo ":: Found container - $CNAME_Clinet"
-    if [ "$(docker ps -q -f name=$CNAME_Clinet)" ]; then
-        echo ":: Stopping running container - $CNAME_Clinet"
-        docker stop $CNAME_Clinet;
-    fi
-    echo ":: Removing stopped container - $CNAME_Clinet"
-    docker rm $CNAME_Clinet;
-fi
-
-CNAME_db="db-kpis-agile"
-if [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
-    echo ":: Found container - $CNAME_db"
-    if [ "$(docker ps -q -f name=$CNAME_db)" ]; then
-        echo ":: Stopping running container - $CNAME_db"
-        docker stop $CNAME_db;
-    fi
-    echo ":: Removing stopped container - $CNAME_db"
-    docker rm $CNAME_db;
-fi
