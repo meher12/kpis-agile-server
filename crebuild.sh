5e1cd84160953a7a8600d6b6925e1db808c4842f
meher12 <meher.khemiri@gmail.com>
2022-04-29 11:43:36 +0200

    update bash script

--- crebuild.sh
+++ crebuild.sh
@@ -5,10 +5,10 @@ CNAME_Clinet="kpis-agile-ui"
 CNAME_db="db-kpis-agile"
 if [ "$(docker ps -qa -f name=$CNAME_Server)" || "$(docker ps -qa -f name=$CNAME_Clinet)" || "$(docker ps -qa -f name=$CNAME_db)" ]; then
     echo ":: Found container - $CNAME_Server"
-    if [ "$(docker ps -q -f name=$CNAME_Server)"  || "$(docker ps -q -f name=$CNAME_Clinet)" || "$(docker ps -q -f name=$CNAME_db)"]; then
+    if [ "$(docker ps -q -f name=$CNAME_Server)"  || "$(docker ps -q -f name=$CNAME_Clinet)" || "$(docker ps -q -f name=$CNAME_db)" ]; then
         echo ":: Stopping running container --"
-        docker-compose stop;
-        docker-compose up --no-recreate;
+        docker stop CNAME_db CNAME_Clinet CNAME_Server;
+        docker-compose start;
     else
       docker-compose up;    
     fi
