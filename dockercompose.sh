9aa101402fc18c6ae4279adc14be980af272af17
meher12 <meher.khemiri@gmail.com>
2022-04-30 12:19:30 +0200

    update docker-compose file bash

--- dockercompose.sh
+++ dockercompose.sh
@@ -6,7 +6,7 @@ if [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
    
         echo ":: Down service backend and frontend service- "
         #docker-compose down kpis-ui kpis-backend;
-        docker-compose up kpis-ui kpis-backend;
+        docker-compose up -V kpis-ui kpis-backend;
 else
       echo ":: Up all service -"
       docker-compose up;
