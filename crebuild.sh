aee7f5144fcdb17abb6a0ed041d70dffe5189df9
meher12 <meher.khemiri@gmail.com>
2022-04-30 16:46:05 +0200

    update rebuild bash file

--- crebuild.sh
+++ crebuild.sh
@@ -3,9 +3,10 @@ docker network prune
 CNAME_Server="kpis-agile-server"
 CNAME_Client="kpis-agile-ui"
 CNAME_db="db-kpis-agile"
+
 if [ "$(docker ps -qa -f name=$CNAME_Server)" ] || if [ "$(docker ps -qa -f name=$CNAME_Client)" ] || [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
     echo ":: Found container - $CNAME_Server"
-    if [ "$(docker ps -q -f name=$CNAME_Server)" ] || [ "$(docker ps -q -f name=$CNAME_Client)" ] ||  [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
+    if [ "$(docker ps -q -f name=$CNAME_Server)" ] || [ "$(docker ps -q -f name=$CNAME_Client)" ] ||  [ "$(docker ps -q -f name=$CNAME_db)" ]; then
         echo ":: Stopping running container -"
         docker stop $CNAME_Server $CNAME_Client $CNAME_db;
     fi
