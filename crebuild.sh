0579e63395144d37e17104b945ec4f44d33b4263
meher12 <meher.khemiri@gmail.com>
2022-04-30 16:42:04 +0200

    update rebuild bash file

--- crebuild.sh
+++ crebuild.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 docker network prune
 CNAME_Server="kpis-agile-server"
-if [ "$(docker ps -qa -f name=$CNAME_Server)" ]; then
-    echo ":: Found container - $CNAME_Server"
-    if [ "$(docker ps -q -f name=$CNAME_Server)" ]; then
-        echo ":: Stopping running container - $CNAME_Server"
-        docker stop $CNAME_Server;
-    fi
-    echo ":: Removing stopped container - $CNAME_Server"
-    docker rm $CNAME_Server;
-fi
-
 CNAME_Client="kpis-agile-ui"
-if [ "$(docker ps -qa -f name=$CNAME_Client)" ]; then
-    echo ":: Found container - $CNAME_Client"
-    if [ "$(docker ps -q -f name=$CNAME_Client)" ]; then
-        echo ":: Stopping running container - $CNAME_Client"
-        docker stop $CNAME_Client;
-    fi
-    echo ":: Removing stopped container - $CNAME_Client"
-    docker rm $CNAME_Client;
-fi
-
 CNAME_db="db-kpis-agile"
-if [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
-    echo ":: Found container - $CNAME_db"
-    if [ "$(docker ps -q -f name=$CNAME_db)" ]; then
-        echo ":: Stopping running container - $CNAME_db"
-        docker stop $CNAME_db;
+if [ "$(docker ps -qa -f name=$CNAME_Server)" ] || if [ "$(docker ps -qa -f name=$CNAME_Client)" ] || [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
+    echo ":: Found container - $CNAME_Server"
+    if [ "$(docker ps -q -f name=$CNAME_Server)" ] || [ "$(docker ps -q -f name=$CNAME_Client)" ] ||  [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
+        echo ":: Stopping running container -"
+        docker stop $CNAME_Server $CNAME_Client $CNAME_db;
     fi
-    echo ":: Removing stopped container - $CNAME_db"
-    docker rm $CNAME_db;
+    echo ":: Removing stopped container - "
+    docker rm $CNAME_Server $CNAME_Client $CNAME_db;
 fi
