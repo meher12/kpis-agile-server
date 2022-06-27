00a0ed89ae354fef7c0880407e328bb7639d3dc7
meher12 <meher.khemiri@gmail.com>
2022-06-27 18:11:57 +0100

    update docker compose command

--- dockercompose.sh
+++ dockercompose.sh
@@ -9,13 +9,15 @@ if [ "$(docker ps -qa -f name=$CNAME_db)" ]; then
         echo ":: No recreate service db, backend and frontend service- "
         #docker-compose run --service-ports db;
         #docker-compose stop;
-        docker-compose up --no-recreate;
+        #docker-compose up --no-recreate;
+        docker compose up --no-recreate;
         #docker-compose run --service-ports kpis-backend;
         #docker-compose  stop;
         #docker-compose -f docker-composetwo.yml up;
 else
       echo ":: Up all service -"
-      docker-compose up;
+      #docker-compose up;
+      docker compose up;
   
     
 fi
